2e03521baa891c3e4404c1c1fe227be85d9d9f45
hilmimarifin <hilmi.m.arifin@gmail.com>
2021-01-03 12:24:00 +0700

    adding contact icon on bottom screen

--- components/layout/index.js
+++ components/layout/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import Head from 'next/head'
 import styled from 'styled-components'
 import Navbar from './navbar'
+import Image from 'next/image'
 import Burger from './navbar/Burger'
 
 const StyledLayout = styled.div`
@@ -15,6 +16,15 @@ const StyledLayout = styled.div`
     .content {
         flex-grow: 2;
         margin-top: 90px;
+        .contact {
+            width: 50px;
+            height: 50px;
+            border-radius: 100px;
+            text-align: center;
+            position: fixed;
+            bottom: 45px;
+            right: 10px;
+        }
         }
     footer {
         width: 100%;
@@ -39,6 +49,12 @@ const Layout = ({children, title}) => {
                 </header>
                 <div className="content">
                     {children}
+                    <div className="contact">
+                        <a href={`https://api.whatsapp.com/send?phone=6283844982006&`} target="_blank">
+                            <Image src="/icons/whatsapp.svg" height="40" width="40"></Image>
+                        </a>
+
+                    </div>
                 </div>
 
 
